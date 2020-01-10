a302575ec3584ba11d9ba886756e9b18e1f55165
gmemmy <gmemmy22@gmail.com>
2020-01-10 19:18:34 +0100

    feature: implement game logic

--- src/view/index.js
+++ src/view/index.js
@@ -3,12 +3,47 @@ import './index.css';
 import Board from '../components/board'
 
 class Game extends Component {
-  render() {
+  sanitizeInput(input, dimension) {
+    let output;
+    output = parseInt(input);
+    if (Number.isNaN(output)) {
+      return {
+        valid: false,
+        msg: `${input} not valid. 
+        Please enter a valid integer for ${dimension}`
+      };
+      } else if (output < 0) {
+        return {
+          valid: false,
+          msg: `${input} not valid. 
+          Please enter a positive integer for ${dimension}`
+        };
+      } else {
+        return {
+          valid: true,
+          output
+        };
+    }
+  }
+    render() {
+      let width = this.sanitizeInput(
+        prompt("Please enter board width"),
+        'width'
+      );
+      while (!width.valid) {
+        width = this.sanitizeInput(prompt(width.msg), 'width');
+      }
+  
+      let height = this.sanitizeInput(
+        prompt("Please enter board height"),
+        'height'
+      );
+      while (!height.valid) {
+        height = this.sanitizeInput(prompt(height.msg), 'height');
+      }  
     return (
       <div className="game">
-        <div className="game-board">
-          <Board width={10} height={10} />
-        </div>
+        <Board width={width.output} height={height.output} />
       </div>
     );
   }
